e81edb09fa201462fe835d9fe6ee618b5eab9245
Juan M. Cortéz <juanm.cortez@gmail.com>
2024-08-07 08:37:36 -0300

    :tada: Carevise initial commit!

---
+++ database/seeders/Users/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders\Users;
+
+use App\Models\Users\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+    }
+}
