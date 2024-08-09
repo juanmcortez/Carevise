2d1d79368d753766de88f22ea06023f091f8af13
juanmcortez <juanm.cortez@gmail.com>
2024-08-09 02:00:11 -0300

    🔧 New layout and partials

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,14 +1,40 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <style>
+        [x-cloak] {
+            display: none
+        }
+    </style>
+
+    <!-- Scripts -->
+    @vite(['resources/css/Carevise.css', 'resources/js/Carevise.js'])
 </head>
 
-<body>
-    @yield('content')
+<body id="guest">
+    <div id="container">
+        @include('layouts.partials.header')
+        <div id="main">
+            <div id="contentholder">
+                <main id="content">
+                    @yield('content')
+                </main>
+                @include('layouts.partials.footer')
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,5 @@
+<div id="sidebar">
+    <div id="sdbrTop">SDBR T</div>
+    <div id="sdbrMiddle">SDBR M</div>
+    <div id="sdbrBottom">SDBR B</div>
+</div>
