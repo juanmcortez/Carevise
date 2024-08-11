9084a6e300c0ce0212b79418cc8d4856344ca7d4
juanmcortez <juanm.cortez@gmail.com>
2024-08-11 00:22:37 -0300

    🔧 Profile on register

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Users\UserController;
 
 // Protected routes by login
 Route::middleware('auth')->group(function () {
+    // Landing
     Route::get('/', function () {
         return view('main.index');
     })->name('index');
+
+    // User Profile
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/{username}/profile', 'edit')->name('user.profile');
+    });
 });
