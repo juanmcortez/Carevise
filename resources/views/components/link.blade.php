5b20842a9361c5bb73a58c46ede8ddcfb431f476
juanmcortez <juanm.cortez@gmail.com>
2024-08-12 15:48:19 -0300

    🔧 New User menu

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,6 +1,5 @@
 @props(['route' => '/', 'title' => '', 'target' => '_self'])
-<a href="{{ $route }}"
-    {{ $attributes->merge(['class' => 'text-xs font-medium text-teal-500 transition duration-150 ease-in-out xl:text-sm hover:text-teal-500 focus:outline-none focus:underline']) }}
-    title="{{ $title }}" target="{{ $target }}">
+<a id="link" href="{{ $route }}" {{ $attributes->merge(['class' => '']) }} title="{{ $title }}"
+    target="{{ $target }}">
     {{ $slot }}
 </a>
