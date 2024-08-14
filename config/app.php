65f220bbc5050f68e55a2b93bb2c98a51bb102b4
juanmcortez <juanm.cortez@gmail.com>
2024-08-14 10:11:58 -0300

    🔧 New company config file

--- config/app.php
+++ config/app.php
@@ -4,19 +4,6 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Application Name
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of your application, which will be used when the
-    | framework needs to place the application's name in a notification or
-    | other UI elements where an application name needs to be displayed.
-    |
-    */
-
-    'name' => env('APP_NAME', 'Laravel'),
-
-    /*
-    |--------------------------------------------------------------------------
     | Application Environment
     |--------------------------------------------------------------------------
     |
