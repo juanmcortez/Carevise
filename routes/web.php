4e8d3d973fdf256e434d93a23e9313f89de6b710
juanmcortez <juanm.cortez@gmail.com>
2024-08-23 18:26:36 -0300

    🔧 User + profile CRUD improvements

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ Route::middleware('auth')->group(function () {
     Route::controller(UserController::class)->name('users.')->group(
         function () {
             Route::get('/users', 'index')->name('list');
+            Route::get('/users/create', 'create')->name('create');
+            Route::post('/users/create', 'store')->name('store');
             Route::get('/users/{user}/profile', 'edit')->name('profile');
             Route::patch('/users/{user}/profile', 'update')->name('profile.update');
+            Route::put('/users/password/update', 'passwordupdate')->name('profile.password.update');
+            Route::delete('/users/remove', 'destroy')->name('profile.destroy');
         }
     );
 });
