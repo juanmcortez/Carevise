542b10336065d6f525aeaa3dd3d2be69039e3154
juanmcortez <juanm.cortez@gmail.com>
2024-08-26 13:28:43 -0300

    🔧 Table component

---
+++ resources/views/components/tools/simple-table.blade.php
@@ -0,0 +1,40 @@
+@props([
+    'colnu' => 1,
+    'colbl' => [],
+    'colwd' => [],
+    'coldt' => [],
+])
+@php
+    if ($colnu == 1 && count($colbl) > 1) {
+        $colnu = count($colbl);
+    }
+@endphp
+<table id="table">
+    <thead>
+        <tr>
+            @for ($idx = 0; $idx < $colnu; $idx++)
+                <th class="{{ $colwd[$idx] }}">
+                    @if (!empty($colbl[$idx]))
+                        {{ __($colbl[$idx]) }}
+                    @else
+                        &nbsp;
+                    @endif
+                </th>
+            @endfor
+        </tr>
+    </thead>
+
+    <tbody>
+        {{ $slot }}
+    </tbody>
+
+    @if (!empty($coldt))
+        <tfoot>
+            <tr>
+                <td colspan="{{ $colnu }}">
+                    {{ $coldt->links() }}
+                </td>
+            </tr>
+        </tfoot>
+    @endif
+</table>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,74 +7,15 @@
 @endsection
 
 @section('content')
-    <table id="table">
-        <thead>
-            <tr>
-                <th class="w-3/12">
-                    <span>{{ __('Name') }}
-                        {{-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                            <polygon points="16,4 23,11 21.6,12.4 16,6.8 10.4,12.4 9,11 "></polygon>
-                            <rect width="32" height="32" fill="none"></rect>
-                        </svg> --}}
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                            <polygon points="16,28 9,21 10.4,19.6 16,25.2 21.6,19.6 23,21 "></polygon>
-                            <rect width="32" height="32" fill="none"></rect>
-                        </svg>
-                    </span>
-                </th>
-                <th class="w-3/12">
-                    <span>{{ __('E-mail') }}
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                            <polygon points="16,4 23,11 21.6,12.4 16,6.8 10.4,12.4 9,11 "></polygon>
-                            <polygon points="16,28 9,21 10.4,19.6 16,25.2 21.6,19.6 23,21 "></polygon>
-                            <rect width="32" height="32" fill="none"></rect>
-                        </svg>
-                    </span>
-                </th>
-                <th class="w-3/12">
-                    <span>{{ __('Username') }}
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                            <polygon points="16,4 23,11 21.6,12.4 16,6.8 10.4,12.4 9,11 "></polygon>
-                            <polygon points="16,28 9,21 10.4,19.6 16,25.2 21.6,19.6 23,21 "></polygon>
-                            <rect width="32" height="32" fill="none"></rect>
-                        </svg>
-                    </span>
-                </th>
-                <th class="w-2/12">
-                    <span>{{ __('Birthdate') }}
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                            <polygon points="16,4 23,11 21.6,12.4 16,6.8 10.4,12.4 9,11 "></polygon>
-                            <polygon points="16,28 9,21 10.4,19.6 16,25.2 21.6,19.6 23,21 "></polygon>
-                            <rect width="32" height="32" fill="none"></rect>
-                        </svg>
-                    </span>
-                </th>
-                <th class="w-1/12">
-                    <span>{{ __('Age') }}
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                            <polygon points="16,4 23,11 21.6,12.4 16,6.8 10.4,12.4 9,11 "></polygon>
-                            <polygon points="16,28 9,21 10.4,19.6 16,25.2 21.6,19.6 23,21 "></polygon>
-                            <rect width="32" height="32" fill="none"></rect>
-                        </svg>
-                    </span>
-                </th>
+    <x-tools.sortable-table class="user-list" :colwd="['w-3/12', 'w-3/12', 'w-3/12', 'w-2/12', 'w-1/12']" :colbl="['Name', 'E-mail', 'Username', 'Birthdate', 'Age']">
+        @foreach ($users as $user)
+            <tr x-data x-on:click="window.location.href='{{ route('users.profile', ['user' => $user->username]) }}'">
+                <td>{{ $user->demographic->complete_name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->demographic->date_of_birth->format('M d, Y') }}</td>
+                <td>{{ $user->demographic->age }}</td>
             </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user)
-                <tr x-data x-on:click="window.location.href = '{{ route('users.profile', ['user' => $user->username]) }}'">
-                    <td class="!text-left">{{ $user->demographic->complete_name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->username }}</td>
-                    <td>{{ $user->demographic->date_of_birth->format('M d, Y') }}</td>
-                    <td>{{ $user->demographic->age }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="5">{{ $users->links() }}</td>
-            </tr>
-        </tfoot>
-    </table>
+        @endforeach
+    </x-tools.sortable-table>
 @endsection
